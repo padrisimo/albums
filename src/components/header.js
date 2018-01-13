5d8ce6957d47ad7d0f3910b8864bee154d8409ca
padrisimo <nopuedoparardemolar@outlook.com>
2018-01-13 11:05:02 +0100

    header styling

--- src/components/header.js
+++ src/components/header.js
@@ -2,21 +2,30 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 const Header = () => {
-    const { textStyle, viewStyle } = styles;
+  const { textStyle, viewStyle } = styles;
 
-    return (
-        <View style={viewStyle}>
-            <Text style={textStyle}>ALBUMS</Text>
-        </View>
-    );
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>ALBUMS</Text>
+    </View>
+  );
 };
 
 const styles = {
   viewStyle: {
-    backgroundColor: '#9E9E9E'
+    backgroundColor: '#78909C',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.9,
+    elevation: 2,
+    position: 'relative'
   },
   textStyle: {
-    fontSize: 20
+    fontSize: 20,
+    color: '#FFFFFF'
   }
 };
 
