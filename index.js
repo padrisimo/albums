5a38762802f41fe18258f4f79d7be41e98cc79aa
padrisimo <nopuedoparardemolar@outlook.com>
2018-01-13 11:15:59 +0100

    making header reusable

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from './src/components/header';
 
 const App = () => (
-    <Header />
+    <Header headerText={'Albums'} />
 );
 
 AppRegistry.registerComponent('lealbums', () => App);
