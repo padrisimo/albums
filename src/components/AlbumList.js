d90d7245dd925849c365a5ea00bb2f44d9dacf7d
padrisimo <nopuedoparardemolar@outlook.com>
2018-01-13 19:26:18 +0100

    component level state

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,10 +3,11 @@ import { View, Text, } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+    state = { albums: [] };
 
     componentWillMount() {
         axios.get('http://www.mocky.io/v2/5a59e0ff2e0000930d71fadc')
-            .then(response => console.log(response));
+            .then(response => this.setState({ albums: response.data }));
     }
 
     render() {
