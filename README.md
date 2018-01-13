900549746447238e9207c8c4a9f114a6c53246bd
padrisimo <nopuedoparardemolar@outlook.com>
2018-01-13 09:49:59 +0100

    first shot

---
+++ README.md
@@ -0,0 +1,20 @@
+# React Native Albums
+
+React Native simple albums app
+
+### After cloning
+
+```
+> yarn
+> npm start
+```
+
+in other terminal (to run in android device or emulator)
+
+```
+> react-native run-android
+```
+
+### Misc
+
+Common Troubleshooting cases [here](http://facebook.github.io/react-native/docs/running-on-device.html)
---
+++ src/components/header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = () => {
+    const { textStyle, viewStyle } = styles;
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>ALBUMS</Text>
+        </View>
+    );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8'
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
+
+export default Header;
