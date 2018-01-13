4c494ec5b06ce48f7fca1df8e44dbc27340ce86e
padrisimo <nopuedoparardemolar@outlook.com>
2018-01-13 19:13:08 +0100

    network request

---
+++ src/components/AlbumList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, Text, } from 'react-native';
+import axios from 'axios';
+
+class AlbumList extends Component {
+
+    componentWillMount() {
+        axios.get('http://www.mocky.io/v2/5a59e0ff2e0000930d71fadc')
+            .then(response => console.log(response));
+    }
+
+    render() {
+        return (
+            <View>
+                <Text> Album listaaaa! </Text>
+            </View>
+        );
+    }
+}
+
+export default AlbumList;
