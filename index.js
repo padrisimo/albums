f2e1c7d3971653e24f51165ffeb51964f27e70cd
padrisimo <nopuedoparardemolar@outlook.com>
2018-01-15 15:37:15 +0100

    making content scrollable

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-    <View>
-        <Header headerText={'Albums'} />
+    <View style={{ flex: 1 }}>
+        <Header headerText={'Records'} />
         <AlbumList />
     </View>
 );
